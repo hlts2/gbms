451b99affa78f00d0659a88d4c43cebb939f8bd1
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-09-25 17:01:07 +0900

    added: process to create bms table

--- gbms.go
+++ gbms.go
@@ -1,6 +1,8 @@
 package gbms
 
-import "unsafe"
+import (
+	"unsafe"
+)
 
 // Search Searches a pattern in a text and returns count of pattern
 func Search(str, ptn string) int {
@@ -10,4 +12,19 @@ func Search(str, ptn string) int {
 // toBytes convert string to bytes
 func toBytes(str string) []byte {
 	return *(*[]byte)(unsafe.Pointer(&str))
+}
+
+func bmsTable(str, ptn []byte) map[byte]int {
+	table := make(map[byte]int, len(str))
+	for i := 0; i < len(str); i++ {
+		j := len(ptn) - 1
+		for j >= 0 {
+			if str[i] == ptn[j] {
+				break
+			}
+			j--
+		}
+		table[str[i]] = j
+	}
+	return table
 }
--- gbms_test.go
+++ gbms_test.go
@@ -1,9 +1,79 @@
 package gbms
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func assert(t *testing.T, b bool) {
 	if !b {
 		t.Fail()
+	}
+}
+
+func TestBmsTable(t *testing.T) {
+	tests := []struct {
+		input struct {
+			str []byte
+			ptn []byte
+		}
+		expected map[byte]int
+	}{
+		{
+			input: struct {
+				str []byte
+				ptn []byte
+			}{
+				str: []byte("Lil and Tiny"),
+				ptn: []byte("Tiny"),
+			},
+			expected: map[byte]int{
+				'L': -1, 'i': 1, 'l': -1, ' ': -1,
+				'a': -1, 'n': 2, 'd': -1,
+				'T': 0, 'y': 3,
+			},
+		},
+		{
+			input: struct {
+				str []byte
+				ptn []byte
+			}{
+				str: []byte("Lil"),
+				ptn: []byte("l"),
+			},
+			expected: map[byte]int{
+				'L': -1, 'i': -1, 'l': 0,
+			},
+		},
+		{
+			input: struct {
+				str []byte
+				ptn []byte
+			}{
+				str: []byte(""),
+				ptn: []byte(""),
+			},
+			expected: map[byte]int{},
+		},
+		{
+			input: struct {
+				str []byte
+				ptn []byte
+			}{
+				str: []byte("a"),
+				ptn: []byte("abc"),
+			},
+			expected: map[byte]int{
+				'a': 0,
+			},
+		},
+	}
+
+	for i, test := range tests {
+		got := bmsTable(test.input.str, test.input.ptn)
+
+		if !reflect.DeepEqual(test.expected, got) {
+			t.Errorf("tests[%d] - bmsTable is wrong. expected: %v, got: %v", i, test.expected, got)
+		}
 	}
 }
